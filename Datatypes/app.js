3089b15f493e5d63f593ced2187d56f687935370
Gurudevkini <kinigurudev049@gmail.com>
2025-09-14 14:08:59 +0000

    added datatypes

---
+++ Datatypes/app.js
@@ -0,0 +1,24 @@
+// Primitive types
+// 7 Types : String , Number , Boolearn , nulll ,undefined , symbol 
+// ,BigInt
+   const Score =100 
+   const scoreValue = 100.3
+
+ // Reference(Non Primitve )
+ // Array , Objects , functions
+  /* const score = 100
+   const scorevalue = 100.3
+ 
+   const isLoggedIn = false
+   const outsideTemp = null
+   let userEmail ;
+  const id= symbol()
+  const bigNumber = 8731228338739;*/
+// why array object are non primitive 
+
+const heros = ["shaktiman", "naagraj " , "spiderman "]
+let a = {
+    name : "Gurudev",
+    age : 22,
+}
+ console.log(a);
